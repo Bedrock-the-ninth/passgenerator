a7b42e641e882de54054b5122fce6f8347a77efe
Bedrock-the-ninth <ravi.be2002@gmail.com>
2023-07-10 04:45:57 +0430

    The logic is finalized, only the styling is left

--- main.js
+++ main.js
@@ -1,28 +1,109 @@
-//Imports here
+class PasswordGenerator {
+  constructor() {
+    this.nums = "0123456789";
+    this.alphabetLow = "abcdefghijklmnopqrstuvwxyz";
+    this.alphabetUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.specChars = "!@#$%^&*()";
+  }
 
-let nums = "0123456789";
-let alphabetLow = "abcdefghijklmnopqrstuvwxyz";
-let alphabetUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let specChars = "!@#$%^&*()";
-
-// let shuffled = str.split('').sort(function(){return 0.5-Math.random()}).join('');
-
-let createBtn = document.getElementById("createPass");
-let copyBtn = document.getElementById("copyPass");
-
-const createPass = () => {
-  let passProp = {
-    passLength: document.getElementById("passLength").value,
-    lowCase: document.getElementById("lowCase").checked,
-    upCase: document.getElementById("upCase").checked,
-    number: document.getElementById("number").checked,
-    specChar: document.getElementById("specChar").checked,
-    numberOfUp: document.getElementById("upsNumber").value,
-    numberOfNum: document.getElementById("numsNumber").value,
-    numberOfSpec: document.getElementById("specsNumber").value,
+  getRandomChars = (charSet, numberOfChars) => {
+    const chars = [];
+    for (let i = 0; i < numberOfChars; i++) {
+      const randomIndex = Math.floor(Math.random() * charSet.length);
+      chars.push(charSet[randomIndex]);
+    }
+    return chars.join("");
   };
 
-  document.getElementById("demo").innerHTML = JSON.stringify(passProp);
-};
+  checkboxCheck = () => {
+    // These few statments are to remove the number input selectors from the DOM if the checkbox is not checked
+    document.getElementById("upCase").addEventListener("change", () => {
+      document.getElementById("upsNumber").disabled =
+        !document.getElementById("upCase").checked;
+    });
 
-createBtn.addEventListener("click", createPass);
+    document.getElementById("number").addEventListener("change", () => {
+      document.getElementById("numsNumber").disabled =
+        !document.getElementById("number").checked;
+    });
+
+    document.getElementById("specChar").addEventListener("change", () => {
+      document.getElementById("specsNumber").disabled =
+        !document.getElementById("specChar").checked;
+    });
+  };
+
+  // contentCheck = () => {
+  //   document.getElementById("password").addEventListener("change", () => {
+  //     document.getElementById("copyPass").disabled = !;
+  //   });
+  // };
+
+  generatePassword = () => {
+    const passLength = parseInt(document.getElementById("passLength").value);
+    const includeUpperCase = document.getElementById("upCase").checked;
+    const numUpperCase = parseInt(document.getElementById("upsNumber").value);
+    const includeNumbers = document.getElementById("number").checked;
+    const numNumbers = parseInt(document.getElementById("numsNumber").value);
+    const includeSpecialChars = document.getElementById("specChar").checked;
+    const numSpecialChars = parseInt(
+      document.getElementById("specsNumber").value
+    );
+
+    let password = "";
+
+    if (includeUpperCase) {
+      password += this.getRandomChars(this.alphabetUp, numUpperCase);
+    }
+
+    if (includeNumbers) {
+      password += this.getRandomChars(this.nums, numNumbers);
+    }
+
+    if (includeSpecialChars) {
+      password += this.getRandomChars(this.specChars, numSpecialChars);
+    }
+
+    const remainingLength = passLength - password.length;
+    if (remainingLength > 0) {
+      password += this.getRandomChars(this.alphabetLow, remainingLength);
+    }
+
+    const shuffledPassword = password
+      .split("")
+      .sort(() => 0.5 - Math.random())
+      .join("");
+
+    if (shuffledPassword.length > passLength) {
+      alert("Wrong inputs");
+    } else {
+      console.log(shuffledPassword);
+      document.getElementById("password").value = shuffledPassword;
+    }
+  };
+
+  copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(document.getElementById("password").value)
+      .then(() => {
+        alert("Successfully copied to clipboard!");
+      })
+      .catch(() => {
+        alert("Not succesful!");
+      });
+  };
+}
+
+const passwordGenerator = new PasswordGenerator();
+
+passwordGenerator.checkboxCheck();
+
+const createBtn = document.getElementById("createPass");
+createBtn.addEventListener("click", () => {
+  passwordGenerator.generatePassword();
+});
+
+const copyBtn = document.getElementById("copyPass");
+copyBtn.addEventListener("click", () => {
+  passwordGenerator.copyToClipboard();
+});
